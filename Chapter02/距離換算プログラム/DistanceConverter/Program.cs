80babbd5082f84e68ecee30bf018ba0f4593f03b
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-04-22 11:26:03 +0900

    コマンドライン引数の対応

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -1,19 +1,33 @@
-﻿namespace DistanceConverter {
+﻿
+namespace DistanceConverter {
     internal class Program {
         static void Main(string[] args) {
-
-            if(args.Length >= 1 && args[0] == "-tom") {
-                //フィートからメートルへの対応表を出力
-                for (int feet = 1; feet <= 10; feet++) {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+            if(args.Length >= 1) {
+                if ( args[0] == "-tom") {
+                    PrintFeetToMeterList(1, 10);
+                } else if (args[0] == "-tof") {
+                    PrintMeterToFeetList(1, 10);
+                } else {
+                    Console.WriteLine("引数エラー");
                 }
             } else {
-                // メートルからフィートへの対応表を出力
-                for (int meter = 1; meter <= 10; meter++) {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine($"{meter}m = {feet:0.0000}ft");
-                }
+                Console.WriteLine("引数エラー");
+            }
+        }
+
+        // フィートからメートルへの対応表を出力
+        static void PrintFeetToMeterList(int start, int stop) {
+            for (int feet = start; feet <= stop; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+            }
+        }
+
+        // メートルからフィートへの対応表を出力
+        static void PrintMeterToFeetList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
             }
         }
 
---
+++ Chapter04/文字列を数値に変換する/Code4_36/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Code4_36 {
+    internal class Program {
+        static void Main(string[] args) {
+            var str = "1234";
+
+            if (int.TryParse(str, out int height)) {
+                Console.WriteLine(height + ":変換成功");
+            } else {
+                Console.WriteLine("変換できません");
+            }
+
+            //Parseで変換する場合は例外処理が必要となる
+            try {
+                int num = int.Parse(str);
+            }
+            catch (Exception) {
+                Console.WriteLine("変換できません");
+            }
+        }
+    }
+}
