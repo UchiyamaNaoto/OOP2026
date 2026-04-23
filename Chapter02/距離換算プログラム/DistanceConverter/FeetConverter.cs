aacb422cbe6f288ce71b25c724a11ad563698280
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-04-23 10:29:10 +0900

    constからstatic readonlyへ変更

--- Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 
 namespace DistanceConverter {
     public static class FeetConverter {
+        private static readonly double ratio = 0.3048;    //定数
 
         // メートルからフィートを求める
         public static double FromMeter(double meter) {
-            return meter / 0.3048;
+            return meter / ratio;
         }
 
         //フィートからメートルを求める
         public static double ToMeter(double feet) {
-            return feet * 0.3048;
+            return feet * ratio;
         }
     }
 }
