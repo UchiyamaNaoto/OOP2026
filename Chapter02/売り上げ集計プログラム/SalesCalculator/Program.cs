5f923e714cf05b8029090d8154b1b34d6677e9cb
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-05-07 11:19:16 +0900

    SalesCounterクラスの変更

--- Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
@@ -3,26 +3,12 @@
 namespace SalesCalculator {
     internal class Program {
         static void Main(string[] args) {
-            var sales = new SalesCounter( ReadSales(@"data\sales.csv"));
+
+            var sales = new SalesCounter( @"data\sales.csv");
             Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
             foreach (KeyValuePair<string,int> obj in amountPerStore) {
                 Console.WriteLine($"{obj.Key} {obj.Value}");
             }
-        }
-
-        static List<Sale> ReadSales(string filePath) {
-            List<Sale> sales = new List<Sale>();    //リスト
-            string[] lines = File.ReadAllLines(filePath);
-            foreach(string line in lines) {
-                string[] items = line.Split(',');   //カンマ区切りで分割
-                Sale sale = new Sale {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2]),
-                };
-                sales.Add(sale);
-            }
-            return sales;
         }
     }
 }
--- Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
@@ -9,9 +9,25 @@ namespace SalesCalculator {
     public class SalesCounter {
         private readonly List<Sale> _sales;
 
+
         //コンストラクタ－
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
+        }
+
+        public List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();    //リスト
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');   //カンマ区切りで分割
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2]),
+                };
+                sales.Add(sale);
+            }
+            return sales;
         }
 
         //店舗別売り上げを求める
