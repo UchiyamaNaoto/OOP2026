13ef26f1cda3c5bf733d6c0f440d58e2b3e62d64
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-05-07 12:06:04 +0900

    P57まで

--- Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
@@ -5,8 +5,8 @@ namespace SalesCalculator {
         static void Main(string[] args) {
 
             var sales = new SalesCounter( @"data\sales.csv");
-            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string,int> obj in amountPerStore) {
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore) {
                 Console.WriteLine($"{obj.Key} {obj.Value}");
             }
         }
--- Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     //売り上げ集計クラス
     public class SalesCounter {
-        private readonly List<Sale> _sales;
+        private readonly IEnumerable<Sale> _sales;
 
 
         //コンストラクタ－
@@ -15,7 +15,7 @@ namespace SalesCalculator {
             _sales = ReadSales(filePath);
         }
 
-        public List<Sale> ReadSales(string filePath) {
+        public IEnumerable<Sale> ReadSales(string filePath) {
             List<Sale> sales = new List<Sale>();    //リスト
             string[] lines = File.ReadAllLines(filePath);
             foreach (string line in lines) {
@@ -31,8 +31,8 @@ namespace SalesCalculator {
         }
 
         //店舗別売り上げを求める
-        public Dictionary<string, int> GetPerStoreSales() {
-            Dictionary<string, int> dict = new Dictionary<string, int>();
+        public IDictionary<string, int> GetPerStoreSales() {
+            var dict = new Dictionary<string, int>();
 
             foreach (var sale in _sales) {
                 //既に店舗名が辞書のキーに登録されているか？
