1a3b6a81fde57d2eb7e7411d41c30bf48a28198a
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-05-13 11:51:07 +0900

    コード3.1

---
+++ Chapter03/ラムダ式とLINQ/Section01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var count = Count(5);
+            Console.WriteLine(count);
+        }
+
+        static int Count(int num) {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var count = 0;
+            foreach (var n in numbers) {
+                if (n == num) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
