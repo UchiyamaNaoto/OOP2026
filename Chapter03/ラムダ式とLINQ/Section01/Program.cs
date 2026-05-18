c0c339f0c5028a1bbb1122c83c73aa16b72db556
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-05-18 11:44:15 +0900

    ラムダ式とは P68

--- Chapter03/ラムダ式とLINQ/Section01/Program.cs
+++ Chapter03/ラムダ式とLINQ/Section01/Program.cs
@@ -1,16 +1,18 @@
 ﻿namespace Section01 {
     internal class Program {
-        static void Main(string[] args) {
 
-            var count = Count(5);
+        static void Main(string[] args) {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+
+
+            var count = Count(numbers, n => n % 4 == 0 || n % 5 == 0);
             Console.WriteLine(count);
         }
 
-        static int Count(int num) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+        static int Count(int[] numbers, Predicate<int> judge) {
             var count = 0;
             foreach (var n in numbers) {
-                if (n == num) {
+                if (judge(n) == true) {
                     count++;
                 }
             }
