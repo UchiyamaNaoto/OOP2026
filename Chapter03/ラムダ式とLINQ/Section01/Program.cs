7f082b17e5ea1f06ab8d730893ef4a4337542515
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-05-20 10:35:27 +0900

    P71まで

--- Chapter03/ラムダ式とLINQ/Section01/Program.cs
+++ Chapter03/ラムダ式とLINQ/Section01/Program.cs
@@ -2,21 +2,26 @@
     internal class Program {
 
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
 
-
-            var count = Count(numbers, n => n % 4 == 0 || n % 5 == 0);
-            Console.WriteLine(count);
-        }
-
-        static int Count(int[] numbers, Predicate<int> judge) {
-            var count = 0;
-            foreach (var n in numbers) {
-                if (judge(n) == true) {
-                    count++;
-                }
-            }
-            return count;
+            //以下の条件を満たすものが存在するか調べる
+            //【教科書P134～137を参考に】
+            //・文字数が6文字以上
+            //・o を含む
+            //・最後が n
+            var exists = cities.Exists(s => 6 <= s.Length && s.Contains('o') && s.EndsWith('n'));
+            Console.WriteLine(exists);
         }
     }
 }
+
+
