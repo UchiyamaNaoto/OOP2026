6671ed7da7e8873441742e2c6f4013d31d754b66
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-05-25 11:16:13 +0900

    ２つの要素を入れ替える

--- Chapter04/nullに関するイディオム/Section04/Program.cs
+++ Chapter04/nullに関するイディオム/Section04/Program.cs
@@ -34,12 +34,24 @@ namespace Section04 {
             int? amount = sale?.Amount;
             Console.WriteLine("売上高：" + amount);
             #endregion
+
+            #region ２つの要素を入れ替える
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            Console.Write("入れ替え前：");
+            Console.WriteLine("a = " + a + " b = " + b);
+
+            (b, a, c) = (a, b, 999);    //入れ替え
+
+            Console.Write("入れ替え後：");
+            Console.WriteLine("a = " + a + " b = " + b + " c = " + c);
+            #endregion
         }
 
         private static string? DefaultMessage() {
             return "DefaultMessage";
         }
-
         private static string? GetMessage(string code) {
             return code;
         }
