7a38e4e77e5d05fe272a4e31bd3f3f81dd5fd913
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-05-27 11:58:50 +0900

    P112問題4.2 テンプレート

---
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<string> langs = [
+                "C#", "Java", "Ruby", "PHP", "Python", "TypeScript",
+                "JavaScript", "Swift", "Go",
+            ];
+
+            Exercise1(langs);
+            Console.WriteLine("---");
+            Exercise2(langs);
+            Console.WriteLine("---");
+            Exercise3(langs);
+        }
+
+        private static void Exercise1(List<string> langs) {
+            Console.WriteLine(" --- 4.1.1 ---");
+
+            //foreach文
+            Console.WriteLine("foreach文で出力");
+            foreach (var lang in langs) {
+                if (lang.Contains('S'))
+                    Console.WriteLine(lang);
+            }
+
+            //for文
+            Console.WriteLine("\nfor文で出力");
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains('S'))
+                    Console.WriteLine(langs[i]);
+            }
+
+            //while文
+            Console.WriteLine("\nwhile文で出力");
+            int count = 0;
+            while (count < langs.Count) {
+                if (langs[count].Contains('S'))
+                    Console.WriteLine(langs[count]);
+                count++;
+            }
+        }
+
+        private static void Exercise2(List<string> langs) {
+            Console.WriteLine("\n --- 4.1.2 ---");
+            //LINQを使用する（Where）
+            var selected = langs.Where(s => s.Contains('S'));
+            foreach (var lang in selected) {
+                Console.WriteLine(lang);
+            }
+        }
+
+        private static void Exercise3(List<string> langs) {
+            Console.WriteLine("\n --- 4.1.3 ---");
+            var lang = langs.Find(s => s.Length == 10) ?? "unknown";
+            Console.WriteLine(lang);
+        }
+    }
+}
---
+++ Chapter04/演習問題/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(" --- 4.2.1 ---");
+            Exercise1();
+            Console.WriteLine("\n --- 4.2.2 ---");
+            Exercise2();
+            Console.WriteLine("\n --- 4.2.3 ---");
+            Exercise3();
+        }
+
+        //P89～90を参考にする
+        private static void Exercise1() {
+            //if-else文を使用（コード4.9）
+            
+        }
+
+        private static void Exercise2() {
+            //switch文を使用（コード4.10）
+
+        }
+
+        private static void Exercise3() {
+            //switch式を使用（コード4.11）
+
+
+        }
+    }
+}
