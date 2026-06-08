60fcb31dcab2ac402c8b6210239ab499af52a123
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-06-08 11:17:59 +0900

    5章まで終了

---
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -0,0 +1,14 @@
+﻿using Exercise01;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var ym1 = new YearMonth(2002,10);
+            var ym2 = new YearMonth(2002,10);
+            if (ym1 == ym2)
+                Console.WriteLine("等しい");
+            else
+                Console.WriteLine("等しくない");
+        }
+    }
+}
