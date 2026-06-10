94a4c5721f3a347c51e853bdd15184a94e1f5624
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-06-10 10:23:27 +0900

    問題6.2のテンプレートまで

---
+++ Chapter06/文字列の比較/Section01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var str1 = "こまんど";
+            var str2 = "ｺﾏﾝﾄﾞ";
+
+            var cultureInfo = new CultureInfo("ja-JP");
+
+            if (String.Compare(str1, str2,cultureInfo,
+                                    CompareOptions.IgnoreWidth | CompareOptions.IgnoreKanaType) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+
+            if (String.Compare(str1, str2, ignoreCase:true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
