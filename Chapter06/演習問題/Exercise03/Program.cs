58d375d931ead20d2c1baca6708b4151cd49fc8c
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-06-11 11:26:09 +0900

    配列を用いた集計

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -73,11 +73,17 @@ namespace Exercise03 {
                 Console.WriteLine(word.Key + ":" + word.Value);
             }
 
-
+            Console.WriteLine();    //改行
 
             //配列を用いた集計
+            var array = Enumerable.Repeat(0, 26).ToArray();
+            foreach (var alph in str) {
+                array[alph - 'a']++;
+            }
 
-
+            for(char ch = 'a'; ch <= 'z'; ch++) {
+                Console.WriteLine($"{ch}:{array[ch - 'a']}");
+            }
 
             //'a'から順にカウントして集計
 
