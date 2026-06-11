f90d1a3acd67e0505b869466c645608c12555263
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-06-11 10:49:07 +0900

    辞書（ディクショナリ）を使った集計 その１

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -57,9 +57,22 @@ namespace Exercise03 {
 
         //アルファベットの数をカウントして表示する
         private static void Exercise6(string text) {
-
+            var str = text.ToLower().Replace(" ", "");
 
             //辞書（ディクショナリ）を使った集計
+            //var alphDicCount = Enumerable.Range('a', 26).
+            //    ToDictionary( num => ((char)num).ToString(),num => 0 );
+
+            var dict = new SortedDictionary<char, int>();
+            foreach (var c in str) {
+                if (dict.ContainsKey(c))
+                    dict[c]++;      //登録されている場合
+                else
+                    dict[c] = 1;    //未登録の場合
+            }
+            foreach (var word in dict) {
+                Console.WriteLine(word.Key + ":" + word.Value);
+            }
 
 
 
