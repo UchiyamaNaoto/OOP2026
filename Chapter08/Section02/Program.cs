3802a899675bcab03c3300165abd6b6339d01d33
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-06-29 15:49:27 +0900

    第８章演習問題準備

---
+++ Chapter08/Section02/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.txt");
+            var we = new WordsExtractor(lines);
+            foreach (var word in we.Extract()) {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+            Exercise2(text);
+        }
+
+        private static void Exercise1(string text) {
+            
+        }
+
+        private static void Exercise2(string text) {
+            
+        }
+    }
+}
