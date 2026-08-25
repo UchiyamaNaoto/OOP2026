ed0182b1cef190b431f24d81fc22fb6e08ff0959
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-08-25 15:33:24 +0900

    Settingsクラスのsingleton対応

--- FormApps/CarReportSystem/Form1.cs
+++ FormApps/CarReportSystem/Form1.cs
@@ -11,7 +11,7 @@ namespace CarReportSystem {
         BindingList<CarReport> listCarReports = new BindingList<CarReport>();
 
         //設定クラスのオブジェクトを生成
-        Settings settings = new Settings();
+        //Settings settings = Settings.Instance;
 
         public Form1() {
             InitializeComponent();
@@ -32,7 +32,7 @@ namespace CarReportSystem {
                         if( serializer.Deserialize(reader) is Settings loadedSettings) {
                             settings = loadedSettings;
                             //背景色設定
-                            BackColor = Color.FromArgb(settings.MainFormBackColor);
+                            BackColor = Color.FromArgb(Settings.Instance.MainFormBackColor);
                         }
                     }
                 }
@@ -223,7 +223,7 @@ namespace CarReportSystem {
             if (cdColor.ShowDialog() == DialogResult.OK) {
                 BackColor = cdColor.Color;
                 //変更された色の情報を保存
-                settings.MainFormBackColor = cdColor.Color.ToArgb();
+                Settings.Instance.MainFormBackColor = cdColor.Color.ToArgb();
             }
         }
 
@@ -233,8 +233,8 @@ namespace CarReportSystem {
             //P284以降を参考にする（ファイル名：setting.xml）
 
             using (var writer = XmlWriter.Create("setting.xml")) {
-                var serializer = new XmlSerializer(settings.GetType());
-                serializer.Serialize(writer, settings);
+                var serializer = new XmlSerializer(Settings.Instance.GetType());
+                serializer.Serialize(writer, Settings.Instance);
             }
         }
 
--- FormApps/CarReportSystem/Settings.cs
+++ FormApps/CarReportSystem/Settings.cs
@@ -1,7 +1,19 @@
 ﻿namespace CarReportSystem {
-    public class Settings {
+    public sealed class Settings {
+
+        //唯一のSettingオブジェクト
+        private static readonly Settings _instance = new Settings();
+
         //メイン画面に設定した色情報
         public int MainFormBackColor { get; set; }
             = SystemColors.Control.ToArgb();
+
+        //唯一のオブジェクトを取得する
+        public static Settings Instance {
+            get { return _instance; }
+        }
+
+        //外部からnewできないようにする
+        private Settings() { }
     }
 }
