8ea84cede843996cf2f544b0e859152e647af8b5
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-08-25 14:01:43 +0900

    不要なusingと表記を整理

--- FormApps/CarReportSystem/Settings.cs
+++ FormApps/CarReportSystem/Settings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarReportSystem {
+﻿namespace CarReportSystem {
     public class Settings {
         //メイン画面に設定した色情報
         public int MainFormBackColor { get; set; }
