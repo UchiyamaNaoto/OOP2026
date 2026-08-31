74313336a6456c472eaa67e5529bc9481320cff5
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2026-08-31 13:31:15 +0900

    Productクラス作成

--- FormApps/SQLiteProductSample/Product.cs
+++ FormApps/SQLiteProductSample/Product.cs
@@ -2,7 +2,19 @@ using System.ComponentModel;
 
 namespace SQLiteProductSample;
 
+// 商品1件分のデータを表すクラス
 public class Product
 {
+    //商品ID（自動採番）
+    [DisplayName("ID")]
+    public int Id { get; set; }
 
+    //商品名
+    [DisplayName("商品名")]
+    public string Name { get; set; } = string.Empty;
+
+    //商品価格
+    [DisplayName("価格")]
+    public int Price { get; set; }
 }
+
